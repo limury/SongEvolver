674cb193e5e86f4fd2ef4885d0e0049d9c57c7d4
Lorenzo James Bernasconi <ljb818@ic.ac.uk>
2022-11-04 15:58:24 +0000

    removed deleted file references

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,16 +1,10 @@
 import { configureStore } from '@reduxjs/toolkit'
 import { setupListeners } from '@reduxjs/toolkit/dist/query'
-import { pokemonApi } from '../services/pokemon'
-import { spotifyApi } from '../services/spotify'
-import counterReducer from './counter/counterSlice'
 import authReducer from './authSlice'
 import spotifyReducer from './spotifySlice'
 
 const store = configureStore({
   reducer: {
-    counter: counterReducer,
-    [pokemonApi.reducerPath]: pokemonApi.reducer,
-    [spotifyApi.reducerPath]: spotifyApi.reducer,
     auth: authReducer,
     spotify: spotifyReducer,
   },
